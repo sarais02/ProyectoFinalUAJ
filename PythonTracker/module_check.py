3b00d27fcaf522da63c2d490db71e67d25d33530
PabloArrNowak <82083153+PabloArrNowak@users.noreply.github.com>
2024-06-26 12:52:40 +0200

    Rama re-hecha: .bat, instrucciones en README, comprobador de módulos,  JSON configuración nombres mapas

---
+++ PythonTracker/module_check.py
@@ -0,0 +1,20 @@
+import importlib.util
+import sys
+
+modulos = ["os", "json", "pandas", "numpy", "matplotlib"]
+
+modulos_faltan = []
+
+for modulo in modulos:
+    if importlib.util.find_spec(modulo) is None:
+        modulos_faltan.append(modulo)
+
+if modulos_faltan:
+    print("Faltan los siguientes módulos:")
+    for modulo in modulos_faltan:
+        print(f"- {modulo}")
+    print("\nInstálalos ejecutando este comando:")
+    print(f"pip install {' '.join(modulos_faltan)}")
+    sys.exit(1)
+else:
+    sys.exit(0)
